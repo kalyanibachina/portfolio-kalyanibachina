ecedfd38f8a960b45689c56d44700fbfd5db8d46
kalyani Bachina <kalyanibachina2005@gmail.com>
2025-04-18 09:54:10 -0400

    UI-HomePage-Finished

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -8,30 +8,30 @@ function Home(){
     return(
         <div className="container-fluid">
             <div className="container d-flex justify-content-center align-items-center min-vh-100">
-                <div className="row border bg-white shadow box-area">
+                <div className="row border bg-white shadow rounded box-area">
                     <div className="col-md-6 d-flex flex-column justify-content-center align-items-center left-box">
-                        <div className="mb-3">
-                            <img src="kalyani.jpg" alt="kalyani" height="auto" width="120px"/>
+                        <div className="mb-3 pt-5">
+                            <img src="kalyani.jpg" alt="kalyani" height="auto" width="120px" className="rounded"/>
                         </div>
                         <div className="mb-2 d-flex flex-column align-items-center">
-                            <h3 className="fs-4 fw-bolder mb-0">Kalyani Bachina</h3>
-                            <p className="mb-0 fw-bold"><small>(she/her)</small></p>
-                            <p><small>Software Engineer</small></p>
+                            <h3 className="fs-4 fw-bolder lh-1 mb-0 text-wrap">Kalyani Bachina</h3>
+                            <p className="mb-1 fw-bolder"><small>(she/her)</small></p>
+                            <p className=""><small>Software Engineer</small></p>
                         </div>
-                        <div className="mb-3">
+                        <div className="mb-3 pt-2">
                             <a href="#"><button className="btn btn-dark button">Email Me</button></a>
                         </div>
-                        <div className="d-flex fs-3">
-                            <a href="https://www.linkedin.com/in/bachinakalyani/" className="nav-link me-2"><FaLinkedin /></a>
-                            <a href="#" className="nav-link me-2"><FaGithub /></a>
-                            <a href="#" className="nav-link me-2"><FaSquareInstagram /></a>
+                        <div className="d-flex fs-4">
+                            <a href="https://www.linkedin.com/in/bachinakalyani/" target="_blank" className="nav-link me-2"><FaLinkedin /></a>
+                            <a href="#" target="_blank" className="nav-link me-2"><FaGithub /></a>
+                            <a href="#" target="_blank" className="nav-link me-2"><FaSquareInstagram /></a>
                         </div> 
                     </div>
-                    <div className="col-md-6 d-flex flex-column justify-content-center p-5 right-box">
+                    <div className="col-md-6 d-flex flex-column justify-content-center p-5 rounded-end right-box">
                         <h2 className="fs-1 fw-bolder pb-3">About Me</h2>
                         <p className="mb-5 pb-3 lh-sm fst-italic"><small>I am a passionate technologist with a Master’s in Computer Science, specializing in building scalable and robust software solutions. I value clean architecture, efficient code, and continuous growth through hands-on problem-solving and learning.</small></p>
                         <div className="d-flex justify-content-center">
-                            <a><button className="btn btn-dark me-4 button">Resume</button></a>
+                            <a href="/KALYANIBresume-QA.pdf" target="_blank"><button className="btn btn-dark me-4 button" >Resume</button></a>
                             <a href="/portfolio"><button className="btn btn-dark button">Portfolio</button></a>
                         </div>
                     </div>
--- src/Portfolio/PortfolioHome.js
+++ src/Portfolio/PortfolioHome.js
@@ -1,10 +1,23 @@
 import Header from "./Header";
+import '../css/PortfolioHome.css'
 
 function PortfolioHome(){
     return(
+       <div className="container-fluid">
         <div>
             <Header />
         </div>
+        <div className="container d-flex justify-content-center align-items-center">
+            <div className="row">
+                <div className="col-md-6 d-flex flex-column justify-content-center align-items center">
+                    left-box
+                </div>
+                <div className="col-md-6">
+                    right-box
+                </div>
+            </div>
+        </div>
+       </div>
     )
 }
 
